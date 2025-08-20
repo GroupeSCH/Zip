c4d962ebd16187379abe62fc543a6a8dd52b783c
GroupeSCH <gestion@mtlzipline.com>
2025-08-20 17:11:57 -0400

    type json to text in getting data

--- netlify/functions/get-dispos.js
+++ netlify/functions/get-dispos.js
@@ -2,7 +2,7 @@ export default async function handler(req, context) {
   const { getStore } = await import('@netlify/blobs');
   try {
     const store = getStore('global');
-    const dispos = await store.get('dispo.json', { type: 'json' }) ?? {};
+    const dispos = await store.get('dispo.json', { type: 'text' }) ?? {};
 
     return new Response(JSON.stringify(dispos), {
       status: 200,
