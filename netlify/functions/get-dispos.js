c2bb6446ba66b50d81d3ecdb0a5790132494c5a5
GroupeSCH <gestion@mtlzipline.com>
2025-08-20 14:48:49 -0400

    return Response

--- netlify/functions/get-dispos.js
+++ netlify/functions/get-dispos.js
@@ -1,16 +1,17 @@
-
 export default async function handler(req, context) {
   const { getStore } = await import('@netlify/blobs');
-
   try {
     const store = getStore('global');
-    const dispos = await store.get('dispo.json', {type: 'json'}) ?? {};
-    console.log(dispos);
-    return {
-      statusCode : 200,
-      body: JSON.stringify(dispos)
-    };
+    const dispos = await store.get('dispos.json', { type: 'json' }) ?? {};
+
+    return new Response(JSON.stringify(dispos), {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    });
   } catch (error) {
-    return { statusCode : 500, body: 'Erreur interne: ' + error.message}
+    return new Response(
+      JSON.stringify({ error: "Erreur interne", message: error.message }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
   }
 }
